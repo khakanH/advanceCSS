6f96f9815daadc928f507d3844236aacb5626d84
danishalikhan688 <56578933+danishalikhan688@users.noreply.github.com>
2020-06-12 16:41:09 +0500

    Conversion to scss , Code optimization , new Scss Structuring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './assets/css/app.css';
+// import './assets/css/app.css';
+import './assets/scss/main.scss';
 import Header from './components/header';
 import State from './context/state';
 function App() {
